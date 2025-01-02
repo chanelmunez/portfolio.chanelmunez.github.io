0eab20953062f287ed155182387bd63004383ed3
chanel munezero <chanelmunez@gmail.com>
2025-01-02 09:58:50 -0600

    -amuse right extension, clean up api routes

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -2,10 +2,10 @@ import { getRequestContext } from '@cloudflare/next-on-pages'
 
 export const runtime = 'edge'
 
-/*
-export async function GET() {
-  //const responseText = 'get sadlkjf '
-  const responseText = {'hey':'you guys'} 
+//export default function onRequest({ res }) {
+export default function onRequest() {
+  const data = { message: "asklfdskjfslfj" };
+  return new Response("stuff!")
 
   // In the edge runtime you can use Bindings that are available in your application
   // (for more details see:
@@ -16,18 +16,8 @@ export async function GET() {
   // KV Example:
   // const myKv = getRequestContext().env.MY_KV_NAMESPACE
   // await myKv.put('suffix', ' from a KV store!')
-  // const suffix = await myKv.get('suffix')
   // return new Response(responseText + suffix)
-
-  return new Response(responseText)
-}
-*/
-
-//export default function onRequest({ res }) {
-export default function onRequest() {
-  const data = { message: "asklfdskjfslfj" };
-  //res.status(200).json(data);
-  return new Response("stuff!")
+  // const suffix = await myKv.get('suffix')
 }
 
 
